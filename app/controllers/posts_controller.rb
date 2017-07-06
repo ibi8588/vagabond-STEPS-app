b70edbe9d8ba0423f7e6bf614771b834dc27f5f7
kat tsukuda <bearheartsbat@gmail.com>
2017-07-06 14:30:12 -0700

    add post edit route, controller, view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,6 +16,16 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update_attributes(post_params)
+    redirect_to post_path(@post)
+  end
+
   private
 
   def post_params
