069a0c28f9b5dcf9568810aeac364dc511430f38
Ibrahim Aldridge <ibi8588@Ibrahims-MacBook-Pro.local>
2017-07-06 11:37:34 -0700

    added associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :posts
+  has_many :cities, through: :posts
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170706182521) do
+ActiveRecord::Schema.define(version: 20170706183231) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,10 @@ ActiveRecord::Schema.define(version: 20170706182521) do
     t.string "photo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.bigint "city_id"
+    t.index ["city_id"], name: "index_posts_on_city_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -42,4 +46,6 @@ ActiveRecord::Schema.define(version: 20170706182521) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "cities"
+  add_foreign_key "posts", "users"
 end
