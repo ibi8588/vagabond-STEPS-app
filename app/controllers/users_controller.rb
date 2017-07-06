dd282144dbc2efb4652cca32ff66ab7edfb90dc9
alyshae <alison.walas@gmail.com>
2017-07-06 15:22:46 -0700

    Users actions & views working properly for index, new, create, show, edit & update.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,42 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new
+    if @user.save
+      #flash notice
+      redirect_to user_path(@user)
+    else
+      #flash error
+      redirect_to new_user_path
+    end
+  end
+
+    def show
+      @user = User.find_by_id(params[:id])
+    end
+
+    def edit
+      @user = User.find_by_id(params[:id])
+    end
+
+    def update
+      @user = User.find_by_id(params[:id])
+      @user.update(user_params)
+      #flash notice
+      redirect_to user_path(@user)
+    end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :profile_pic )
+  end
+
 end
