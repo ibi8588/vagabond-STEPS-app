9b69814629faa4196423ef8a862d63c06f48ccae
alyshae <alison.walas@gmail.com>
2017-07-07 16:31:14 -0700

    City Show pages will render posts with the matching city id

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,10 +1,12 @@
 class CitiesController < ApplicationController
   def index
     @cities = City.all
+
   end
 
   def show
     @city = City.find_by_id(params[:id])
+    @posts = Post.all
   end
 
 end
