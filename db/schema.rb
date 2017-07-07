a3e77de3c05c7d0bf923698c6dfdcb516f6b69b2
kat tsukuda <bearheartsbat@gmail.com>
2017-07-07 14:57:03 -0700

    fix migration for :username

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170707210938) do
+ActiveRecord::Schema.define(version: 20170707212913) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,8 @@ ActiveRecord::Schema.define(version: 20170707210938) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.bigint "city_id"
+    t.string "trip_pic"
+    t.string "author"
     t.index ["city_id"], name: "index_posts_on_city_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
