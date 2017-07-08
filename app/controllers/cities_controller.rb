12a7d823a135102aa29df4b57d27e9b1ab6c93ce
alyshae <alison.walas@gmail.com>
2017-07-08 10:19:35 -0700

    City Show views are properly rendering only the posts for that particular city.

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,12 +1,12 @@
 class CitiesController < ApplicationController
   def index
     @cities = City.all
-
   end
 
   def show
     @city = City.find_by_id(params[:id])
     @posts = Post.all
+    @post = Post.find_by_id(params[:id])
   end
 
 end
