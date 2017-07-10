33c64828da07e9f3524c77def27042c04597e735
kat tsukuda <bearheartsbat@gmail.com>
2017-07-10 12:59:33 -0700

    fix commenter name

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -17,6 +17,8 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
+    @comment.commenter = current_user.first_name
+    @comment.save!
     redirect_to post_path(@post)
   end
 
