41ce59a1d1e6ec66aafff1f273020481107c2939
Ibrahim Aldridge <ibi8588@Ibrahims-MacBook-Pro.local>
2017-07-10 10:44:01 -0700

    updating seed file

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'ed8588@gmail.com'
   layout 'mailer'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+City.destroy_all
+city_list = [
+{ city:"Kauia", state:"Hawaii", country:"USA", photo: ""},
+{ city:"San Francisco", state:"California", country:"USA" },
+{ city:"Paris", state: "Île-de-France", country:"France" },
+{ city:"Kralendijk", state: "Bonaire", country:"Netherlands" },
+{ city: "Puerto Vallarta", state:"Jalisco", country:"Mexico" }
+]
+
+city_list.each do |city, state, country|
+  City.create(city: city, state: state, country: country)
+end
+User.destroy_all
+user_list = [
+  { first_name: "Joe", last_name:"Cool",password_digest: "easy", profile_pic: "http://thecabin.net/sites/default/files/styles/teaser__620x350/public/images/3077531_web1_1024x1024.jpg?itok=1D9QhzEy", email: "ed8588@gmail.com" }
+]
