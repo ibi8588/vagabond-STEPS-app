83b7110e4880dbf7f1f4b227d5b3be5e1a123dfd
younjiwoo <younjiwoo@gmail.com>
2017-07-10 13:45:40 -0700

    added pagination using will_paginate

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :require_login, only: [:new, :edit, :destroy, :update, :create]
 
   def index
-    @posts = Post.all
+    @posts = Post.paginate(:page => params[:page], :per_page => 5).order('created_at DESC')
     @users = User.all
   end
 
